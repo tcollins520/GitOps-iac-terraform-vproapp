6cb1f10a234f5f351ff9dbf50f61916593220a67
tcollins520 <tcollins520@gmail.com>
2026-04-25 21:52:32 -0400

    fix to aws credentials

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,5 @@ variable "clusterName" {
   default     = "vprofile-eks"
 }
 
-## Add more variables as needed for your Terraform configuration, such as VPC settings, node group configurations, etc.
-## apply infrastructure changes only if the terraform plan step was successful, which helps prevent potential issues with applying incomplete or incorrect plans.
-# added exit code 1
+ 
+# fix to aws credentials, terraform version, and apply steps in workflow
