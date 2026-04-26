eb49a125230040e9b426d1efcaab4103a0e8bc9f
tcollins520 <tcollins520@gmail.com>
2026-04-26 00:34:29 -0400

    disabled kms-keys

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 6.28.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.5"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.38"
     }
   }
   # it is recommended to use a remote backend to store the state file, especially when working in a team. Here we are using AWS S3 as the backend for storing the Terraform state file.
@@ -34,6 +34,4 @@ terraform {
 
   required_version = ">= 1.6.3"
 }
-##
-##
-##
+
