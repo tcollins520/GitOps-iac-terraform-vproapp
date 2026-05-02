12d351f22e2a6c6b8d82fd8f8f5d9d3da32785a7
tcollins520 <tcollins520@gmail.com>
2026-05-02 03:37:27 -0400

    add: new cluster

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,16 +7,7 @@ variable "region" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-<<<<<<< HEAD
   default     = "vprofile-eks-cluster"
 }
 
 
-=======
-  default     = "vprofile-eks"
-}
-
-
-# Import and manage EKS addons with Terraform
-# re-run after aunthentication to update the addons.
->>>>>>> 86f22d44e8b5b5b7e2cbec74f46ff261515b8ee1
